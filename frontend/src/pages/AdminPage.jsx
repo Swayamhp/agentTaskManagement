5ed87e9343fa8478b7688e44f3e09d0f9e40b069
Swyamhp <swayamgd0@gmail.com>
2025-05-28 15:01:09 +0530

    added three line

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -89,7 +89,21 @@ const AdminPage = () => {
               className="text-gray-500 focus:outline-none md:hidden"
               onClick={() => setSidebarOpen(true)}
             >
-              {/* Empty button content - you may add hamburger icon here */}
+              <svg
+    className="w-6 h-6"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+              
             </button>
           </div>
 
